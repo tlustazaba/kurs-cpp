267d86e3da692e622cbefa3da8805026866c5bb7
tlustazaba <matikruhlinski@gmail.com>
2025-10-25 16:54:13 +0200

    Create tablice poczatek.cpp

---
+++ tablice poczatek.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+int tablica[5];
+
+cout << "podaj 5 liczb " << endl;
+for(int i=0; i<5; i++)
+
+
+
+
+
+}
+
+
+
