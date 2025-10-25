fd9d95f017c854ef4ded42aa5302be62af47c719
tlustazaba <matikruhlinski@gmail.com>
2025-10-25 14:38:40 +0200

    Create zadanko  2 funkcje.cpp

---
+++ zadanko  2 funkcje.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<cctype>
+using namespace std;
+
+
+int suma(int a, int b) {
+
+	int suma = (a + b);
+	return suma;
+}
+	int main() {
+		cout << suma(6, 7) << endl;
+		cout << suma(67696769, 414141)<<endl;
+
+		return 0;
+
+
+	}
