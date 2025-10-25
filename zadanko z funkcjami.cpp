fc3e13c4cab75b890ff9deaf6123a276dd18e823
tlustazaba <matikruhlinski@gmail.com>
2025-10-25 14:14:58 +0200

    Create zadanko z funkcjami.cpp

---
+++ zadanko z funkcjami.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<cctype>
+using namespace std;
+
+
+void powitanie() {
+	cout << "witaj w swiecie C++!";
+}
+			int main() {
+				powitanie();
+		
+
+		return 0;
+
+
+
+
+
+	}
