f513db7912173091b1043fea6e624edb6ca6e5b3
tlustazaba <matikruhlinski@gmail.com>
2025-10-25 09:53:42 +0200

    Create tabliczka mnozenia.cpp

---
+++ tabliczka mnozenia.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double l;
+	cout << "podaj liczbe" << endl;
+	cin >> l;
+	cout << l << "*1=" << l * 1 << endl;
+	cout << l << "*2=" << l * 2 << endl;
+	cout << l << "*3=" << l * 3 << endl;
+	cout << l << "*4=" << l * 4 << endl;
+	cout << l << "*5=" << l * 5 << endl;
+	cout << l << "*6=" << l * 6 << endl;
+	cout << l << "*7=" << l * 7 << endl;
+	cout << l << "*8=" << l * 8 << endl;
+	cout << l << "*9=" << l * 9 << endl;
+	cout << l << "*10=" << l * 10 << endl;
+		
+	return 0;
+
+}
